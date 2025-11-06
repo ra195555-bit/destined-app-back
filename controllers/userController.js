c6dfb5b4e35eb9d0cee58e63a003dc971e400140
Joao Vitor Assis <JoaaoVitorAssis@proton.me>
2025-11-06 16:47:19 -0400

    Corrindo controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,27 @@
 import User from "../models/user.js";
 
-export const getUserProfile = async (req, res) => {
-  const { otherUserId } = req.params;
+const userController = {
+  getUserProfile: async (req, res) => {
+    const { otherUserId } = req.params;
 
-  try {
-    const user = await User.findById(otherUserId);
+    try {
+      const user = await User.findById(otherUserId);
 
-    if (!user) {
-      return res.status(404).json({ message: "Usuário não encontrado." });
+      if (!user) {
+        return res.status(404).json({ message: "Usuário não encontrado." });
+      }
+
+      const userProfile = {
+        name: user.name,
+        photos: user.photos,
+      };
+
+      res.status(200).json(userProfile);
+    } catch (error) {
+      console.error("Erro ao buscar o perfil do usuário:", error);
+      res.status(500).json({ message: "Erro interno do servidor" });
     }
-
-    const userProfile = {
-      name: user.name,
-      photos: user.photos,
-    };
-
-    res.status(200).json(userProfile);
-  } catch (error) {
-    console.error("Erro ao buscar o perfil do usuário:", error);
-    res.status(500).json({ message: "Erro interno do servidor" });
-  }
+  },
 };
+
+export default userController;
